e97258867a8d06269bbbc7d9ce59a72d64402442
Máté Kerekes <everydaymadnes@gmail.com>
2019-03-21 16:20:04 +0100

    Point calculations now use vectors.

--- src/main/modules/parameter.js
+++ src/main/modules/parameter.js
@@ -1,3 +1,5 @@
+const { Vector } = require('vector2d'); // import { Vector } from 'vector2d';
+
 function mergeRegGroup(...args) {
   const stripped = args.map(group =>
     (group && group[0] === '[' ? group.substr(1, group.length - 2) : group));
@@ -40,37 +42,39 @@ function gridAlfToNum(alf) {
   return alfuc.charCodeAt() - 'A'.charCodeAt() + 1;
 }
 
-function subKeyToGrid(keyPad) {
+function subKeyToGridPoint(keyPad) {
   keyPad -= 1;
   const keyPadX = keyPad % 3;
   const keyPadY = 2 - Math.floor(keyPad / 3);
-  const x = (keyPadX + 0.5) / 3;
-  const y = (keyPadY + 0.5) / 3;
-
-  return { x, y };
+  const gridPoint = new Vector(keyPadX, keyPadY);
+  gridPoint.add(new Vector(0.5, 0.5)).divS(3);
+  return gridPoint;
 }
 
-function subKeyesToGrid(keyPads) {
+function subKeyesToGridPoint(keyPads) {
   if (!keyPads || keyPads.length === 0) {
     keyPads = [5];
   }
 
-  let { x: gridX, y: gridY } = subKeyToGrid(keyPads[0]);
+  const gridPoint = subKeyToGridPoint(keyPads[0]);
   let subKey = 3.0;
   for (let i = 1; i < keyPads.length; i += 1) {
-    const { x, y } = subKeyToGrid(keyPads[i]);
-    gridX += (x - 0.5) / subKey;
-    gridY += (y - 0.5) / subKey;
+    const offset = subKeyToGridPoint(keyPads[i]);
+    offset.subtract(new Vector(0.5, 0.5)).divS(subKey);
+    gridPoint.add(offset);
     subKey *= 3;
   }
 
-  return { x: gridX, y: gridY };
+  return gridPoint;
 }
 
-function gridNumToHMU(grid) {
-  const scaledGrid = grid * gridSizeHMU;
+function gridPointToHMU(gridPoint) {
+  const scaledPoint = gridPoint.clone().mulS(gridSizeHMU);
   const offset = -subKeySizeHMU * 2;
-  return scaledGrid + offset;
+  return gridPoint
+    .clone()
+    .mulS(gridSizeHMU)
+    .add(new Vector(offset, offset));
 }
 
 // QUESTION: Convert to matchToPoint, make separate funciton that parses input?
@@ -78,19 +82,22 @@ function stringToPoint(str) {
   const match = pointRegExp.exec(str);
   if (!match) return null;
 
-  const gridX = gridAlfToNum(match[1]);
-  const gridY = parseInt(match[2], 10);
+  const gridPoint = new Vector(gridAlfToNum(match[1]), parseInt(match[2], 10));
   const gridSubKeyes = match[3]
     .split(subKeyesRegExp)
     .filter(Boolean)
     .map(s => parseInt(s, 10));
-  const { x: offsetX, y: offsetY } = subKeyesToGrid(gridSubKeyes);
+  const offset = subKeyesToGridPoint(gridSubKeyes);
 
   // X, Y calc maybe not here? See above ^
-  const x = gridNumToHMU(gridX + offsetX);
-  const y = gridNumToHMU(gridY + offsetY);
+  const gridPointHMU = gridPoint.clone().add(offset);
 
-  return { str, x, y };
+  return {
+    str,
+    gridPoint,
+    gridSubKeyes,
+    gridPointHMU,
+  };
 }
 
 function stringToDistance(str) {
@@ -98,4 +105,4 @@ function stringToDistance(str) {
   if (!match) return null;
 }
 
-console.log(stringToDistance('z0k3/7/7/7')); // eslint-disable-line
+console.log(stringToPoint('A7k3')); // eslint-disable-line
